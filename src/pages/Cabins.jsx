a8959ff2814b97d7271ee8ecf96e7b90a07bba90
Thanuja Kularathna <thanujakularathna119@gmail.com>
2025-11-17 06:23:30 +0530

    create filter

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -4,13 +4,14 @@ import AddCabin from "../features/cabins/AddCabins";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
 function Cabins() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>filter/sort</p>
+        <CabinTableOperations />
       </Row>
       <Row>
         <CabinTable />

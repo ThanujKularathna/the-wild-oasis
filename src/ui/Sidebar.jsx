2b74988d2256c43f30730a7cb9341fbd3e4b5ef8
thanuja <thanujakularathna119@gmail.com>
2026-02-11 21:14:12 +0530

    remove unwanted file

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
 
-// import Uploader from "../data/Uploader.jsx";
+import Uploader from "../data/Uploader.jsx";
 
 const StyledSidebar = styled.header`
   background-color: var(--color-grey-0);
